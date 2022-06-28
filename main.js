c740bef5fb448b5e940ca69a6cd315e5b9ebe813
vinialves2020 <vinicius.livramentofga@gmail.com>
2022-06-28 09:02:40 -0300

    Movimenaçao

---
+++ main.js
@@ -0,0 +1,30 @@
+const canvas=document.getElementById("myCanvas");
+canvas.whidth= 300;
+
+const ctx = canvas.getContext("2d");
+const road= new Road(canvas.whidth/2,canvas.whidth*0.9);
+const car = new Car(road.getLaneCenter(1),150,45,75,"KEYS");
+const traffic= [new Car(road.getLaneCenter(1),-150,45,75,"DUMMY",2)]
+
+
+animate();
+
+function animate(){
+    for (let i = 0; i < traffic.length; i++) {
+        traffic[i].update(road.borders,[]);
+    }
+    car.update(road.borders,traffic);
+    canvas.height= window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0,-car.y+canvas.height*0.7);
+
+    road.draw(ctx);
+    for (let i = 0; i <traffic.length; i++) {
+        traffic[i].draw(ctx,"red");
+    }
+    car.draw(ctx,"blue");
+    ctx.restore();
+   
+    requestAnimationFrame(animate);
+}
